82875c0bf5fda73af0b8294311eb5429dc279b5b
Wes-White <wesleyrwhite86@gmail.com>
2020-01-02 12:00:55 -1000

    created new blog page

--- gatsby-playgound/src/pages/index.js
+++ gatsby-playgound/src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>
+        I'm' Wesley, a full stack developer living on the beautiful Big Island
+        of Hawaii
+      </h2>
+    </div>
+  )
+}
+
+export default IndexPage
